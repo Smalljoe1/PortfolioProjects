8c5c13913997d058e510795794fa271faef5f83e
Joseph Nongu <nongujoseph@yahoo.com>
2022-04-19 15:34:28 +0100

    Update Covid 19 Deaths -  SQL Data Exploration Project.sql

--- Covid 19 Deaths -  SQL Data Exploration Project.sql
+++ Covid 19 Deaths -  SQL Data Exploration Project.sql
@@ -22,7 +22,7 @@ ORDER BY 1,2;
 SELECT Location, CAST(DATE AS DATE) AS DATE, Population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
 FROM CovidDeaths
 WHERE location LIKE '%Africa%'
-ORDER BY 1,2
+ORDER BY 1,2;
 
 -- Countries with Highest Infection Rate compared to Population
 
@@ -30,7 +30,7 @@ SELECT Location, Population, MAX(total_cases) AS HighestInfectionCount,  MAX((to
 FROM CovidDeaths
 WHERE continent IS NOT NULL 
 GROUP BY continent, Population
-ORDER BY PercentPopulationInfected DESC
+ORDER BY PercentPopulationInfected DESC;
 
 -- Countries with Highest Death Count per Population
 
@@ -38,7 +38,7 @@ SELECT Location, MAX(Total_deaths) AS TotalDeathCount
 FROM CovidDeaths
 WHERE continent IS NOT NULL 
 GROUP BY continent
-ORDER BY TotalDeathCount DESC
+ORDER BY TotalDeathCount DESC;
 
 -- BREAKING THINGS DOWN BY CONTINENT
 
@@ -48,14 +48,14 @@ SELECT continent, MAX(Total_deaths) AS TotalDeathCount
 FROM CovidDeaths
 WHERE continent IS NOT NULL 
 GROUP BY continent
-ORDER BY TotalDeathCount DESC
+ORDER BY TotalDeathCount DESC;
 
 -- GLOBAL NUMBERS
 
 SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/SUM(New_Cases)*100 AS DeathPercentage
 FROM CovidDeaths
 WHERE continent IS NOT NULL 
-ORDER BY 1,2
+ORDER BY 1,2;
 
 -- Total Population vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
@@ -66,5 +66,5 @@ JOIN CovidVaccinations cv
 	ON cd.location = cv.location
 	AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL 
-ORDER BY 2,3
+ORDER BY 2,3;
 
